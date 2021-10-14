018a72a22f27f3a3b961759cdac06ee2ff917c84
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-10-14 18:25:00 +0300

    Set up project file structure

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn employee_data.wsgi --log-file -
---
+++ employee_data_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmployeeDataAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'employee_data_app'
