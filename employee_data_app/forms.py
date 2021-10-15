34b41eabca698a85e1c629481590e8207c217ab8
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-10-15 11:56:30 +0300

    Add user registration form

---
+++ employee_data_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+from .models import *
+from django.forms import Form, ModelForm, DateField, widgets
+
+
+class UserSignUpForm(UserCreationForm):
+    password1 = forms.CharField(label='Enter password', 
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', 
+                                widget=forms.PasswordInput)
+    class Meta:
+        model=User
+        fields=("first_name","middle_name","username","email","password1","password2")
+        help_texts = {
+            "username":None,
+        }
