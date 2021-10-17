6faae4bc2d5a6b3ebca5e8da50f0a67cbeadb109
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-10-17 09:22:16 +0300

    Update urls

--- employee_data_app/urls.py
+++ employee_data_app/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path('profile/',app_views.profile,name='profile'),
     path('update_profile/',app_views.update_profile,name='update_profile'),
 
+    path('add_employee',app_views.add_employee,name='add_employee'),
+    path('employees', app_views.employees,name='employees'),
+    path('update_employee/<int:employee_id>', app_views.update_employee,name='update_employee'),
+    path('delete_employee/<int:employee_id>', app_views.delete_employee,name='delete_employee'),
+    path('employee_details/<int:employee_id>',app_views.employee_details,name='employee_details'),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
