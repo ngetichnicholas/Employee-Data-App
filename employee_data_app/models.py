58a90b70c71d4d1b343d6553fb813369f6391413
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-10-18 12:33:17 +0300

    Add employment duration time

--- employee_data_app/models.py
+++ employee_data_app/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.deletion import CASCADE
 from django.core.validators import FileExtensionValidator
 
+from django.utils import timezone
+import math
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -38,6 +41,72 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.middle_name}"
+    
+    def employment_time(self):
+        now = timezone.now()
+        
+        diff= now - self.employment_date
+
+        if diff.days == 0 and diff.seconds >= 0 and diff.seconds < 60:
+            seconds= diff.seconds
+            
+            if seconds == 1:
+                return str(seconds) +  "second"
+            
+            else:
+                return str(seconds) + " seconds"
+
+            
+
+        if diff.days == 0 and diff.seconds >= 60 and diff.seconds < 3600:
+            minutes= math.floor(diff.seconds/60)
+
+            if minutes == 1:
+                return str(minutes) + " minute"
+            
+            else:
+                return str(minutes) + " minutes"
+
+
+
+        if diff.days == 0 and diff.seconds >= 3600 and diff.seconds < 86400:
+            hours= math.floor(diff.seconds/3600)
+
+            if hours == 1:
+                return str(hours) + " hour"
+
+            else:
+                return str(hours) + " hours"
+
+        # 1 day to 30 days
+        if diff.days >= 1 and diff.days < 30:
+            days= diff.days
+        
+            if days == 1:
+                return str(days) + " day"
+
+            else:
+                return str(days) + " days"
+
+        if diff.days >= 30 and diff.days < 365:
+            months= math.floor(diff.days/30)
+            
+
+            if months == 1:
+                return str(months) + " month"
+
+            else:
+                return str(months) + " months"
+
+
+        if diff.days >= 365:
+            years= math.floor(diff.days/365)
+
+            if years == 1:
+                return str(years) + " year"
+
+            else:
+                return str(years) + " years"
 
 class Supervisor(models.Model):
     name = models.OneToOneField(Employee,blank=True,on_delete=CASCADE)
